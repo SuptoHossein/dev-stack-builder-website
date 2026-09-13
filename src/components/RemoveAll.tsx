08f4bd423110be5659e826d4b0b48c7b8a89a594
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:20:16 +0600

    make component functional

--- src/components/RemoveAll.tsx
+++ src/components/RemoveAll.tsx
@@ -1,11 +1,20 @@
-const RemoveAll = () => {
+import type { Dispatch, SetStateAction } from "react";
+import type { Istack } from "../types/Stack";
+import { toast } from "react-toastify";
+
+interface PropType {
+  technologies: Istack[];
+  setTechnologies: Dispatch<SetStateAction<Istack[]>>;
+}
+
+const RemoveAll = ({ technologies, setTechnologies }: PropType) => {
+  const handleRemoveAll = () => {
+    setTechnologies(technologies = [])
+    toast.warning(`All Technologies are removed.`)
+  }
   return (
-    <div>
-      <div className="border border-rose-400 px-3 py-2  rounded-xl">
-        <h2 className="text-rose-600 text-center font-semibold cursor-pointer">
-          Remove All
-        </h2>
-      </div>
+    <div className="mt-20">
+      <button onClick={handleRemoveAll} className="px-4 py-2 text-rose-800 rounded-xl cursor-pointer w-full border border-rose-400 hover:bg-rose-300 hover:border-rose-400 ">Remove All</button>
     </div>
   );
 };
