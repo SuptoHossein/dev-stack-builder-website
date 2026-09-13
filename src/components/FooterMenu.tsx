696650b3ab9000904d951995d5f21f6e4208a0cb
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:18:07 +0600

    redesign footer menus and move footer content to footer component

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -2,16 +2,16 @@ import Logo from "./../assets/logo-text.png";
 
 const FooterMenu = () => {
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-4 ">
-        <div className="flex flex-col space-y-3">
+    <div className="container mx-auto pt-15">
+      <div className="grid grid-cols-4 content-center">
+        <div className="flex flex-col space-y-6">
           <img src={Logo} alt="" width={150} />
           <p className="text-[#94A3B8]">
             Curated tools, technologies, and resources for developers building
             modern software.
           </p>
 
-          <div className="flex gap-5">
+          <div className="flex gap-5 mt-4">
             <p className="text-[#475569] cursor-pointer hover:text-[#DA2B76] font-semibold">
               GitHub
             </p>
@@ -25,50 +25,53 @@ const FooterMenu = () => {
         </div>
 
         <div className="">
-          <h2>PRODUCT</h2>
-          <ul>
-            <li>
+          <h2 className="mb-5">PRODUCT</h2>
+          <ul className="text-[#94A3B8] space-y-2">
+            <li className="hover:text-[#DA2B76]">
               <a href="">Home</a>
             </li>
-            <li>
+            <li className="hover:text-[#DA2B76]">
               <a href="">Technologies</a>
             </li>
-            <li>
+            <li className="hover:text-[#DA2B76]">
               <a href="">Projects</a>
             </li>
           </ul>
         </div>
 
         <div className="">
-          <h2>COMPANY</h2>
-          <ul>
-            <li>
+          <h2 className="mb-5">COMPANY</h2>
+          <ul className="text-[#94A3B8] space-y-2">
+            <li className="hover:text-[#DA2B76]">
               <a href="">About</a>
             </li>
-            <li>
+            <li className="hover:text-[#DA2B76]">
               <a href="">Contact</a>
             </li>
-            <li>
+            <li className="hover:text-[#DA2B76]">
               <a href="">Careers</a>
             </li>
           </ul>
         </div>
 
         <div className="">
-          <h2>PRODUCT</h2>
-          <ul>
-            <li>
+          <h2 className="mb-5">PRODUCT</h2>
+          <ul className="text-[#94A3B8] space-y-2">
+            <li className="hover:text-[#DA2B76]">
               <a href="">Home</a>
             </li>
-            <li>
+            <li className="hover:text-[#DA2B76]">
               <a href="">Technologies</a>
             </li>
-            <li>
+            <li className="hover:text-[#DA2B76]">
               <a href="">Projects</a>
             </li>
           </ul>
         </div>
       </div>
+      
+
+      <div className="border border-slate-100 mt-20"></div>
     </div>
   );
 };
