d8156cdcf6b90f0f7c7456208768dab11786be14
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:24:14 +0600

    create data.json, EmptyStack component and declear type

---
+++ src/components/EmptyStack.tsx
@@ -0,0 +1,12 @@
+
+const EmptyStack = () => {
+  return (
+    <div>
+      <div className="border border-dashed rounded-xl px-3 py-5 text-center border-slate-200">
+        <h2 className="text-[#94A3B8]">Your stack is empty.</h2>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyStack;
