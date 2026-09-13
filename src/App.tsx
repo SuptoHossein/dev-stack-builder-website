99f6549bca2fbdf340880a3caecb004748d9fcba
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:22:25 +0600

    create promise for fetch data and pass this into Suspense component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import { Suspense } from "react";
 import Footer from "./components/Footer";
 import FooterMenu from "./components/FooterMenu";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
+import type { Istack } from "./types/Stack";
+
+const stackFetch = async () : Promise<Istack[]> => {
+  const res = await fetch("/public/data.json");
+  const data = await res.json();
+  return data;
+};
 
 function App() {
+
   return (
     <>
       <Header />
-      <Hero /> 
-      <Technologies />
-      <div className="border border-slate-100 my-5"></div>
+      <Hero />
+      <Suspense fallback={<div className="text-center text-yellow-500">Loading...</div>}>
+        <Technologies stackPromise={stackFetch()} />
+      </Suspense>
+      <div className="border border-slate-100 my-10"></div>
       <FooterMenu />
       <Footer />
     </>
