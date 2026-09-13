d5e9ba671dfc3bbbf1228c45a4ffe15fb9487945
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:22:59 +0600

    make component functional

--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -1,42 +1,56 @@
 import { FaStar } from "react-icons/fa";
+import type { Istack } from "../types/Stack";
+import { useState, type Dispatch, type SetStateAction } from "react";
+import { toast } from "react-toastify";
 
-const TechnologyCard = () => {
+export interface stackProps {
+  stack: Istack;
+  technologies: Istack[];
+  setTechnologies: Dispatch<SetStateAction<Istack[]>>
+}
+
+const TechnologyCard = ({ stack, technologies, setTechnologies }: stackProps) => {
+
+  const [addToStack, setAddToStack] = useState(false);
+
+  const handleAddtoStack = (stack:Istack) => {
+    setTechnologies([...technologies, stack])
+    setAddToStack(true);
+    toast.success(`${stack.name} is added`)
+  };
   return (
     <div>
-      <div className="border border-slate-200 rounded-xl p-5 relative">
+      <div className={`border ${addToStack ? 'border-rose-300': 'border-slate-200'}  rounded-xl p-5 relative`}>
         <div className="flex flex-col space-y-3">
-          <img
-            src="https://icon.icepanel.io/Technology/svg/React.svg"
-            width={40}
-            alt=""
-          />
-          <h2 className="text-2xl font-bold">React</h2>
-          <p className="text-[#64748B]">
-            A declarative, component-based JavaScript library for building
-            modern user interfaces.
-          </p>
+          <img src={stack.icon} width={40} alt="" />
+          <h2 className="text-2xl font-bold">{stack.name}</h2>
+          <p className="text-[#64748B]">{stack.description}</p>
           <div className="border border-slate-50 mt-6"></div>
         </div>
 
         <div className="mt-3 flex justify-between items-center">
           <p className="text-[#475569] bg-[#F4F7FA] px-2 py-1 rounded-md">
-            Frontend
+            {stack.category}
           </p>
-          <p>Beginner-Friendly</p>
+          <p>{stack.difficulty}</p>
           <p className="flex gap-2 items-center justify-center">
             {" "}
             <span className="text-[#FBBF24]">
               <FaStar />
             </span>
-            4.9
+            {stack.rating}
           </p>
         </div>
 
-        <p className="mt-6 bg-[#0A0F1D] px-4 py-2 rounded-xl text-white cursor-pointer text-center">
-          Add to Stack
-        </p>
-        <p className="absolute top-9 right-9 py-1 bg-rose-100 text-rose-500 border border-rose-300 font-semibold rounded-4xl w-30 text-center">
-          Popular
+        <button
+          onClick={() => handleAddtoStack(stack)}
+          disabled={addToStack}
+          className={`mt-6 text-semibold w-full px-4 py-2 rounded-xl cursor-pointer ${addToStack ? 'bg-rose-200 text-rose-700 border border-rose-600' : 'bg-[#0A0F1D] text-white'}`          }
+        >
+          {addToStack === true ? "Added to Stack" : "Add to Stack"}
+        </button>
+        <p className="absolute top-6 right-9 py-1 bg-rose-100 text-rose-500 border border-rose-300 font-semibold rounded-4xl w-30 text-center">
+          {stack.badge}
         </p>
       </div>
     </div>
