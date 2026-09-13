db8f385d7728be55ac2dc22d774db98822f36312
SuptoHossein <supto.hossein@gmail.com>
2026-09-13 15:59:07 +0600

    design Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { LuCopyright } from "react-icons/lu"
+
+const Footer = () => {
+  return (
+    <div className="container mx-auto">
+        <p><LuCopyright />2026 Dev Stack. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
