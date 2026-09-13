808f3000dadaefbb1808b77a224ebe737b1b358d
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:17:07 +0600

    design footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
-import { LuCopyright } from "react-icons/lu"
+import { LuCopyright } from "react-icons/lu";
 
 const Footer = () => {
   return (
-    <div className="container mx-auto">
-        <p><LuCopyright />2026 Dev Stack. All rights reserved.</p>
-    </div>
-  )
-}
+    <>
+      
+      <div className="container mx-auto flex justify-between text-[#94A3B8]">
+        <p className="flex items-center gap-1 my-10">
+          <LuCopyright />
+          2026 Dev Stack. All rights reserved.
+        </p>
 
-export default Footer
+        <ul className="flex gap-5 cursor-pointer my-10">
+          <li className="hover:text-[#DA2B76]">Privacy</li>
+          <li className="hover:text-[#DA2B76]">Terms</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
