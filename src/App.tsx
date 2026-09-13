8d0fbef50d75883435a4288a3f023a8bfc63da48
SuptoHossein <supto.hossein@gmail.com>
2026-09-13 15:59:54 +0600

    add FooterMenu, Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Footer from "./components/Footer";
+import FooterMenu from "./components/FooterMenu";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Technologies from "./components/Technologies";
@@ -9,6 +11,8 @@ function App() {
       <Hero /> 
       <Technologies />
       <div className="border border-slate-100 my-5"></div>
+      <FooterMenu />
+      <Footer />
     </>
   );
 }
