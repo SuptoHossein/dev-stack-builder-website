c25b04883cb8379823d2fbce2fbf5dd7d3007fa0
Shakhawat Hossein <supto.hossein@gmail.com>
2026-09-13 20:19:28 +0600

    refactor design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Banner from './../assets/banner-stack.png'
 
 const Hero = () => {
   return (
-    <div className="container mx-auto flex justify-between items-center">
+    <div className="container mx-auto flex justify-between items-center my-13">
       <div className="flex flex-col space-y-10">
         <h2 className="font-bold text-7xl ">
           Build Your Ideal <br />
@@ -14,7 +14,7 @@ const Hero = () => {
         </p>
 
         <div className="flex gap-3">
-          <button className="bg-linear-to-r from-[#F9721A] to-[#DB4BA9] px-6 cursor-pointer py-2 rounded-md text-white font-semibold">
+          <button className="bg-linear-to-r from-[#FE5525] via-[#B925A3] to-[#8038E7] px-6 cursor-pointer py-2 rounded-md text-white font-semibold">
             Explore Technologies
           </button>
           <button className="border border-gray-200 cursor-pointer px-10 py-2 rounded-md">
